9577fd189edf1abbc4adec1ba5b462dd0807070f
Varun Mehta <varun.mehta.r@gmail.com>
2021-10-10 19:04:01 -0400

    removed size and added arrows

--- js/main.js
+++ js/main.js
@@ -6,18 +6,17 @@ simpleCart({
   },
 
   // tax: 0.075,
-  // currency: "THB",
+  // currency: "USD",
 
   cartStyle: "table",
 
   cartColumns: [
     { attr: "name" , label: "Name" } ,
     { attr: "price" , label: "Price", view: 'currency' } ,
-    { attr: "size" , label: "Size" } ,
     { attr: "color" , label: "Color" } ,
-    { view: "decrement" , label: false , text: "- 1" } ,
+    { view: "decrement" , label: false , text: "&#9660;" } ,
     { attr: "quantity" , label: "Qty" } ,
-    { view: "increment" , label: false , text: "+ 1" } ,
+    { view: "increment" , label: false , text: "&#9650;" } ,
     { attr: "total" , label: "SubTotal", view: 'currency' } ,
     { view: "remove" , text: "Remove" , label: false }
   ]
