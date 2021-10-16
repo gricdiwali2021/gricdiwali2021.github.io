d3e74fffbf5479ec6cb1830031195608ffe7e309
varunmehta <varun.mehta.r@gmail.com>
2021-10-16 12:39:14 -0400

    fixed card header

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,7 @@
 simpleCart.ready(function() {
   simpleCart.each(function(item, x) {
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Name" value="' + item.get('name') + '">');
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Color" value="' + item.get('color') + '">');
+    $('form').append('<input type="hidden" name="' + item.get('id') + ' ProductId" value="' + item.get('color') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Qty" value="' + item.get('quantity') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Price" value="' + item.get('price') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Total" value="' + item.get('total') + '">');
--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ simpleCart({
   cartColumns: [
     { attr: "name" , label: "Name" } ,
     { attr: "price" , label: "Price", view: 'currency' } ,
-    { attr: "color" , label: "Color" } ,
+    { attr: "color" , label: "Product Id" } ,
     { attr: "quantity" , label: "Qty" } ,
     { attr: "total" , label: "SubTotal", view: 'currency' } ,
     { view: "remove" , text: "Remove" , label: false }
