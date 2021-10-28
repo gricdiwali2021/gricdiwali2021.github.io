eb7fc8f9061eb187852cf1bc2c78823017389e25
varunmehta <varun.mehta.r@gmail.com>
2021-10-28 12:18:21 -0400

    not sure what i did!

--- js/style-picker.js
+++ js/style-picker.js
@@ -2,5 +2,4 @@ jQuery('.style-picker div').click(function() {
   var target = $(this).attr('id');
   $(this).addClass('item_color').siblings().removeClass('item_color');
   $('#' + target).show().siblings('div').hide();
-  $('#select_' + target).show().siblings('div').hide();
 });
