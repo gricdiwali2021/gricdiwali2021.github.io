9b22e11e050ccf050b136e1e65c0c3dfe3e3e0a8
varunmehta <varun.mehta.r@gmail.com>
2021-10-28 14:15:34 -0400

    trying another name

--- js/style-picker.js
+++ js/style-picker.js
@@ -2,4 +2,5 @@ jQuery('.style-picker div').click(function() {
   var target = $(this).attr('id');
   $(this).addClass('item_color').siblings().removeClass('item_color');
   $('#' + target).show().siblings('div').hide();
+  $('#qty-' + target).show().siblings('div').hide();
 });
